35cfa0fde59c3d2a0ab99f66282391cee5154199
Imam Fahrizal <mfahreinheitz@gmail.com>
2021-02-18 15:29:29 +0700

    -add: auth server dan web security config

--- user-management/user.core/src/main/java/com/belajar/user/core/configuration/AxonConfig.java
+++ user-management/user.core/src/main/java/com/belajar/user/core/configuration/AxonConfig.java
@@ -33,8 +33,8 @@ public class AxonConfig {
 
     @Bean
     public MongoClient mongo() {
-        var mongoFactory = new MongoFactory();
-        var mongoSettingsFactory = new MongoSettingsFactory();
+        MongoFactory mongoFactory = new MongoFactory();
+        MongoSettingsFactory mongoSettingsFactory = new MongoSettingsFactory();
         mongoSettingsFactory.setMongoAddresses(Collections.singletonList(new ServerAddress(mongoHost, mongoPort)));
         mongoFactory.setMongoClientSettings(mongoSettingsFactory.createMongoClientSettings());
 
