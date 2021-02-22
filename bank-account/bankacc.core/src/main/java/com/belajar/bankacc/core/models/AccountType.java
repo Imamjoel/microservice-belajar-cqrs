b3a73e2b27d2b17581852788062718c1f0bb2094
Imamjoel <mfahreinheitz@gmail.com>
2021-02-22 22:30:04 +0700

    refactoring

---
+++ bank-account/bankacc.core/src/main/java/com/belajar/bankacc/core/models/AccountType.java
@@ -0,0 +1,5 @@
+package com.belajar.bankacc.core.models;
+
+public enum AccountType {
+    SAVINGS, CURRENT
+}
--- bank-account/bankacc.query.api/src/main/java/com/belajar/bankacc/query/api/BankAccQueryApplication.java
+++ bank-account/bankacc.query.api/src/main/java/com/belajar/bankacc/query/api/BankAccQueryApplication.java
@@ -1,8 +1,10 @@
 package com.belajar.bankacc.query.api;
 
+import com.belajar.bankacc.core.configuration.AxonConfig;
 import org.axonframework.spring.config.AxonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,7 +12,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Import(AxonConfiguration.class)
+@EntityScan(basePackages = "com.belajar.bankacc.core.models")
+@Import({ AxonConfig.class })
 public class BankAccQueryApplication {
 
 	public static void main(String[] args) {
