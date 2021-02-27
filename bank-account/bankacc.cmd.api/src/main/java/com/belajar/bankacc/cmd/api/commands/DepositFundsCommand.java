0ade74a5413b9ca7a4c655f0ba20e181df445f4c
Imamjoel <mfahreinheitz@gmail.com>
2021-02-27 17:03:19 +0700

    -add: Rest Controller deposit funds into bank

--- bank-account/bankacc.cmd.api/src/main/java/com/belajar/bankacc/cmd/api/commands/DepositFundsCommand.java
+++ bank-account/bankacc.cmd.api/src/main/java/com/belajar/bankacc/cmd/api/commands/DepositFundsCommand.java
@@ -4,10 +4,14 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.constraints.Min;
+
 @Data
 @Builder
 public class DepositFundsCommand {
     @TargetAggregateIdentifier
     private String id;
+
+    @Min(value = 1, message = "the deposit account must be greather than")
     private double amount;
 }
---
+++ bank-account/bankacc.cmd.api/src/main/java/com/belajar/bankacc/cmd/api/controllers/DepositFundsController.java
@@ -0,0 +1,39 @@
+package com.belajar.bankacc.cmd.api.controllers;
+
+import com.belajar.bankacc.cmd.api.commands.DepositFundsCommand;
+import com.belajar.bankacc.core.dto.BaseResponse;
+import lombok.var;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(path = "/api/v1/depositFunds")
+public class DepositFundsController {
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public DepositFundsController(CommandGateway command) {
+        this.commandGateway = command;
+    }
+
+    @PutMapping(path = "{id}")
+    @PreAuthorize("hashAuthority('WRITE_PRIVILEGE')")
+    public ResponseEntity<BaseResponse> depositFunds(@PathVariable(value = "id") String id,
+                                                     @Valid @RequestBody DepositFundsCommand command) {
+        try {
+            command.setId(id);
+            commandGateway.send(command);
+
+            return new ResponseEntity<>(new BaseResponse("funds successfully deposited"), HttpStatus.OK);
+        } catch (Exception e) {
+            var safeError = "error while proccessing to deposit fund into bank account for id - " + id;
+            return new ResponseEntity<>(new BaseResponse(safeError), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
